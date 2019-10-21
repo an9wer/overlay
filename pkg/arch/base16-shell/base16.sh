243b1ccaf97ff8df448953a6cf06bdb33f545018
an9wer <an9wer@gmail.com>
2019-10-21 16:06:53 +0800

    [pkg] Refine base16.sh

--- pkg/arch/base16-shell/base16.sh
+++ pkg/arch/base16-shell/base16.sh
@@ -1,38 +1,49 @@
 base16() {
   if (( ${#@} == 0 )); then
-    # Display current scheme
-    echo ${BASE16_THEME:-Unset}
-  elif (( ${#@} > 1 )); then
-    echo "Only support one argument."
-    return 1
-  else
-    # Verify argument $1 which points to some scheme
-    if [[ -f /usr/share/base16-shell/scripts/base16-$1.sh ]]; then
-      export BASE16_THEME=$1
+    if [[ -v BASE16_THEME ]]; then
+      echo "${BASE16_THEME}"
+      base16-resource
+    elif [[ -r ~/.base16 ]]; then
+      head -n1 ~/.bash16
       base16-resource
     else
-      echo "Unknown scheme '$1'."
+      echo unset
       return 1
     fi
+  elif (( ${#@} == 1 )); then
+    echo "$1" > ~/.base16
+    base16-resource
+  else
+    echo "Support only one argument at a time."
+    return 1
   fi
 }
 
 base16-clear() {
   unset BASE16_THEME
-  #TODO: How to unset color 1-16
+  [[ -r ~/.base16 ]] && rm ~/.base16
+  # TODO: Unset color 1-16
   echo "Please restart your shell to make changes effect."
 }
 
 base16-resource() {
+  local base16_theme
   if [[ -v BASE16_THEME ]]; then
-    if [[ -f /usr/share/base16-shell/scripts/base16-$BASE16_THEME.sh ]]; then
-      source /usr/share/base16-shell/scripts/base16-$BASE16_THEME.sh
+    base16_theme=$BASE16_THEME
+  elif [[ -r ~/.base16 ]]; then
+    base16_theme=$(head -n1 ~/.base16)
+  else
+    return 1
+  fi
+
+  if [[ -n $base16_theme ]]; then
+    if [[ -r /usr/share/base16-shell/scripts/base16-$base16_theme.sh ]]; then
+      source /usr/share/base16-shell/scripts/base16-$base16_theme.sh
     else
-      echo "Theme '$BASE16_THEME' doesn't exist."
+      echo "Theme '$base16_theme' doesn't exist."
       return 1
     fi
   else
-    echo "Variable 'BASE16_THEME' is unset."
     return 1
   fi
 }
@@ -42,3 +53,6 @@ complete -W \
   "$(ls /usr/share/base16-shell/scripts/base16-*.sh |
       xargs -n1 basename -s .sh | cut -c 8-)" \
   base16
+
+# Run when starting up
+base16-resource
