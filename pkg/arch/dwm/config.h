8ced120df2988ae07f9ec5d9dc54ac37bfad9983
an9wer <an9wer@gmail.com>
2019-10-21 09:40:51 +0800

    [cheese] Fix color of dmenu when triggered by dwm

--- pkg/arch/dwm/config.h
+++ pkg/arch/dwm/config.h
@@ -56,8 +56,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *dmenupasscmd[] = { "dmenu_pass_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_magenta, "-sf", col_gray4, NULL };
+static const char *dmenupasscmd[] = { "dmenu_pass_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_magenta, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *aupcmd[]   = { "amixer", "set", "Master", "2%+", NULL };
 static const char *adowncmd[] = { "amixer", "set", "Master", "2%-", NULL };
